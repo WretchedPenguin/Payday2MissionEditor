bed6fbefb069b52e0192473765d228d65e973604
Ruben Hamelink <ruben.hamelink@hotmail.com>
2021-02-15 20:13:11 +0100

    Change to javascript

--- MissionEditor2/wwwroot/js/site.js
+++ MissionEditor2/wwwroot/js/site.js
@@ -3,22 +3,8 @@
 var canvas = new LGraphCanvas("#mycanvas", graph);
 
 canvas.resize(window.innerWidth, window.innerHeight);
-window.addEventListener("resize", function (){canvas.resize(); });
+window.addEventListener("resize", function() { canvas.resize(); });
 
-// LiteGraph.clearRegisteredTypes();
+LiteGraph.clearRegisteredTypes();
 
-graph.start();
-
-function testNodeOnExecute() {
-    var a = this.getInputData(0);
-    if (a === undefined)
-        a = 0;
-    var b = this.getInputData(1);
-    if (b === undefined)
-        b = 0;
-    this.setOutputData(0, a + b);
-}
-
-function setOutputData() {
-    this.setOutputData(0, parseFloat(this.properties["Value"]));
-}
+//graph.start();
